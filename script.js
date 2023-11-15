45ca58b1c7d8fea1b705c5ed77d51af336c5d1b5
pudders <ericpudney@gmail.com>
2023-11-15 10:35:23 +0100

    added score, endless waves of aliens

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-let canvas = document.getElementById("play-area");
-let ctx = canvas.getContext("2d");
+const canvas = document.getElementById("play-area");
+const ctx = canvas.getContext("2d");
+const currentscore = document.getElementById("score");
 
 canvas.width = 1200;
 canvas.height = 500;
 
-let alienSpeed = 1000;
-let rowsMoved = 0;
+let alienSpeed = 400;
 
 let bulletFired = false;
+let score = 0;
+let scoreIncrement = 10;
 
 const playericon = new Image();
 playericon.src = "spaceship.svg";
@@ -20,7 +22,7 @@ playericon.addEventListener(
     false,
 );
 
-const aliens = [ {alienXPos: 10, alienYPos: 10, isDead: false}, {alienXPos: 60, alienYPos: 10, isDead: false}, {alienXPos: 110, alienYPos: 10, isDead: false}, {alienXPos: 160, alienYPos: 10, isDead: false}, {alienXPos: 210, alienYPos: 10, isDead: false}, {alienXPos: 260, alienYPos: 10, isDead: false}, {alienXPos: 310, alienYPos: 10, isDead: false}, {alienXPos: 360, alienYPos: 10, isDead: false}, {alienXPos: 410, alienYPos: 10, isDead: false}, {alienXPos: 460, alienYPos: 10, isDead: false} ]
+const aliens = [ {alienXPos: 50, alienYPos: 10, isDead: false}, {alienXPos: 100, alienYPos: 10, isDead: false}, {alienXPos: 150, alienYPos: 10, isDead: false}, {alienXPos: 200, alienYPos: 10, isDead: false}, {alienXPos: 250, alienYPos: 10, isDead: false}, {alienXPos: 300, alienYPos: 10, isDead: false}, {alienXPos: 350, alienYPos: 10, isDead: false}, {alienXPos: 400, alienYPos: 10, isDead: false}, {alienXPos: 450, alienYPos: 10, isDead: false}, {alienXPos: 500, alienYPos: 10, isDead: false} ]
 
 function spawnAliens() {
     for (let baddie of aliens) 
@@ -34,27 +36,24 @@ function spawnAliens() {
         );
     }
 }
-spawnAliens();
 
 function moveAliensRight() {
     let moveAliensRt = setInterval(() => {
+        ctx.clearRect(0, aliens[0].alienYPos - 1, canvas.width, 32);
         for (let baddie of aliens) {
-            ctx.clearRect(baddie.alienXPos, baddie.alienYPos, 31, 31);
             baddie.alienXPos += 10;
         }
         spawnAliens();
         if (aliens[9].alienXPos >= 1150) {
-            clearInterval(moveAliensRt);
+            ctx.clearRect(0, aliens[0].alienYPos - 1, canvas.width, 32);
             for (let baddie of aliens) {
-                ctx.clearRect(baddie.alienXPos, baddie.alienYPos, 31, 31);
                 baddie.alienYPos += 30;
             }
-            rowsMoved++;
-            if (rowsMoved >= 3 && alienSpeed >= 100) {
-                alienSpeed -= 50;
-                rowsMoved = 0;
-            }
             moveAliensLeft();
+            clearInterval(moveAliensRt);
+        }
+        if (!checkWave) {
+            clearInterval(moveAliensRt);
         }
     },
     alienSpeed)
@@ -62,20 +61,21 @@ function moveAliensRight() {
 
 function moveAliensLeft() {
     let moveAliensLt = setInterval(() => {
+        ctx.clearRect(0, aliens[0].alienYPos - 1, canvas.width, 32);
         for (let baddie of aliens) {
-            ctx.clearRect(baddie.alienXPos, baddie.alienYPos, 31, 31);
             baddie.alienXPos -= 10;
         }
         spawnAliens();
         if (aliens[0].alienXPos <= 50) {
-            clearInterval(moveAliensLt);
+            ctx.clearRect(0, aliens[0].alienYPos - 1, canvas.width, 32);
             for (let baddie of aliens) {
-                ctx.clearRect(baddie.alienXPos, baddie.alienYPos, 31, 31);
                 baddie.alienYPos += 30;
             }
-            rowsMoved++;
-            // aliens only speed up after moving right - see function above
             moveAliensRight();
+            clearInterval(moveAliensLt);
+        }
+        if (!checkWave) {
+            clearInterval(moveAliensLt);
         }
     },
     alienSpeed)
@@ -125,20 +125,27 @@ function moveLeft() {
 }
 
 function fire() {
-    let bullet = { x: playerXPos + 22, y: 440, width: 5, height: 15, speed: 10 };
+    const bullet = { x: playerXPos + 22, y: 440, width: 5, height: 15, speed: 10 };
     ctx.fillRect(bullet.x, bullet.y, bullet.width, bullet.height);
-    let shot = setInterval(() => {
+    const shot = setInterval(() => {
         ctx.clearRect(bullet.x, bullet.y, bullet.width, bullet.height);
         bullet.y -= bullet.speed;
         ctx.fillRect(bullet.x, bullet.y, bullet.width, bullet.height);
         for (invader of aliens) {
+            if (!invader.isDead) {
             if (bullet.x > invader.alienXPos && bullet.x < (invader.alienXPos + 30) && bullet.y > invader.alienYPos && bullet.y < (invader.alienYPos + 30)) {
                 invader.isDead = true;
+                score += scoreIncrement;
                 ctx.clearRect(invader.alienXPos, invader.alienYPos, 30, 30);
                 ctx.clearRect(bullet.x, bullet.y, bullet.width, bullet.height);
                 clearInterval(shot);
+                updateScore();
+                if (checkWave()) {
+                    restart();
+                }
                 return;
             } 
+        }
         }
         if (bullet.y <= -20) {
             clearInterval(shot);
@@ -146,4 +153,31 @@ function fire() {
     }, 50)
 }
 
+function updateScore() {
+    currentscore.innerText = "Score: " + score;
+}
+
+function checkWave() {
+    for (let baddie of aliens) {
+        if (!baddie.isDead) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function restart() {
+    for (let i=1; i<=aliens.length; i++) {
+        aliens[i-1].isDead = false;
+        aliens[i-1].alienXPos = (i*50);
+        aliens[i-1].alienYPos = 10;
+    }
+    scoreIncrement += 10;
+    alienSpeed -= 100;
+    spawnAliens();
+    moveAliensRight();
+}
+
+spawnAliens();
 moveAliensRight();
+updateScore();
